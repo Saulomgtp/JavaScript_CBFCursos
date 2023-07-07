285a7f548aa4dd83493d160df0ebed671fc92a32
Saulo Vitor Santos Sousa <saulo_mg_tp@hotmail.com>
2023-07-07 17:06:38 -0300

    Atualização da aula 77 com um timer com alarme pré configurado pelo input, e o disparo do alarme com sinalização e som, juntamente com a possibilidade de parar essa sinalização e o som juntos

--- aula77/aula77.js
+++ aula77/aula77.js
@@ -19,7 +19,37 @@
 // toDateString() = Retorna somente a data
 
 const divRelogio = document.querySelector("#relogio")
+const timer = document.querySelector("#caixaPrincipal")
+const ativar = document.querySelector("#ativar")
+const parar = document.querySelector("#parar")
+const tempoAlarme = document.querySelector("#input")
+const horaAlarme = document.querySelector("#horaAlarme")
 
+const som = new Audio("alarme.mp3")
+som.loop = -1
+
+let ts_atual = null
+let ts_alarme = null
+let alarme_ativado = false
+let alarme_tocando = false
+
+ativar.addEventListener("click", (evt) => {
+    ts_atual = Date.now()
+    ts_alarme = ts_atual + (tempoAlarme.value * 1000) //falando que o tempo de alarme é o instante atual do clique + o tempo predefinido no input
+    alarme_ativado = true
+    const dt_alarme = new Date(ts_alarme)
+    horaAlarme.innerHTML = "Hora do Alarme: " + dt_alarme.getHours() + ":" + dt_alarme.getMinutes() + ":" + dt_alarme.getSeconds()
+})
+parar.addEventListener("click", (evt) => {
+    alarme_ativado = false
+    alarme_tocando = false
+    horaAlarme.innerHTML = "Hora do Alarme: "
+    tempoAlarme.value = 0
+    timer.classList.remove("alarme")
+    som.pause()
+    som.currentTime = 0
+
+})
 const relogio =() => {
     const data = new Date()
 
@@ -34,8 +64,20 @@ const relogio =() => {
 
     const horaCompleta = hora + ":" + minuto + ":" + segundo
     divRelogio.innerHTML = horaCompleta
+
+    if(alarme_ativado && !alarme_tocando) {
+        if(data.getTime() >= ts_alarme) {
+            alarme_tocando = true
+            som.play()
+            timer.classList.add("alarme")
+        }
+    }
 }
 setInterval(relogio, 1000);
+
+
+
+
 
 
 
