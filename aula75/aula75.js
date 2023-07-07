b544d02137dae56cc235f29fdf9b15a1ed61cf6e
Saulo Vitor Santos Sousa <saulo_mg_tp@hotmail.com>
2023-07-07 14:59:34 -0300

    adicionando o promisse a um button

--- aula75/aula75.js
+++ aula75/aula75.js
@@ -1,29 +1,38 @@
 const numero = document.querySelector("#numero")
+const btnPromessa = document.querySelector("#btn_promessa")
 
-let promise = new Promise((ok, nok) => {
-    let resultado = true
-    let tempo = 3000
-
-    setTimeout(() => {
-        if(resultado) {
-            ok("Deu tudo certo")
-        } else {
-            nok("Deu tudo errado")
-        }
-    },tempo)
-
+btnPromessa.addEventListener("click", (evt) => {
+    numero.innerHTML = "Processando...."
+    promessa()
+        
+    .then((retorno) => {
+        numero.innerHTML = retorno
+        numero.classList.remove("erro")
+        numero.classList.add("ok")
+    })
+    .catch((retorno) => {
+        numero.innerHTML = retorno
+        numero.classList.add("erro")
+        numero.classList.remove("ok")
+    })
 })
 
-promise.then((retorno) => {
-    numero.innerHTML = retorno
-    numero.classList.remove("erro")
-    numero.classList.add("ok")
-})
-promise.catch((retorno) => {
-    numero.innerHTML = retorno
-    numero.classList.add("erro")
-    numero.classList.remove("ok")
-})
+const promessa =() => {
+    let promise = new Promise((ok, nok) => {
+        let resultado = true
+        let tempo = 3000
+    
+        setTimeout(() => {
+            if(resultado) {
+                ok("Deu tudo certo")
+            } else {
+                nok("Deu tudo errado")
+            }
+        },tempo)
+    })
+    return promise
+}
 
 
-numero.innerHTML = "Processando..."
+
+ numero.innerHTML = "Esperando"
